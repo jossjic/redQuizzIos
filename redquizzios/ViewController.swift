9a974a4115de35d8453d4daf8ce1aa885826869d
lmao <NIGGY>
2023-11-02 12:54:58 -0600

    round() btn

--- redquizzios/ViewController.swift
+++ redquizzios/ViewController.swift
@@ -9,12 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var registerBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // Test push
+        registerBtn.round()
     }
 
-
+}
+extension UIButton{
+    func round(){
+        let roundPts = self.frame.size.height / 2
+        self.layer.cornerRadius = roundPts
+        self.layer.masksToBounds = true
+    }
 }
 
