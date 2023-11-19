0458ccdde55182439eee29eb1e0707d36ddf4aa5
user247991 <jossjic_03@hotmail.com>
2023-11-18 20:36:48 -0600

    game and login

--- redquizzios/signupController.swift
+++ redquizzios/signupController.swift
@@ -1,11 +1,12 @@
 
 import UIKit
-
+import Firebase
 class singupController: UIViewController {
 //outlets
     @IBOutlet weak var registerBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
     
 //actions
