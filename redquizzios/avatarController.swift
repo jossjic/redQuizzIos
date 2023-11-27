a5ef42f8d51b970f4e96975c04b0790ac58b4f57
vanech20 <estefyvane2007@gmail.com>
2023-11-27 12:37:18 -0600

    avatar controller

--- redquizzios/avatarController.swift
+++ redquizzios/avatarController.swift
@@ -1,17 +1,195 @@
-
 import UIKit
+import Firebase
 
 class avatarController: UIViewController {
-//outlets
+
+    // Outlets
+    @IBOutlet weak var playeraHombre: UIImageView!
+    @IBOutlet weak var pantalonHombre: UIImageView!
+    @IBOutlet weak var zapatosHombre: UIImageView!
+    @IBOutlet weak var editAvatarBtn: UIButton!
+    @IBOutlet weak var maleAvatar: UIImageView!
+    @IBOutlet weak var femaleAvatar: UIImageView!
+    @IBOutlet weak var playeraMujer: UIImageView!
+    @IBOutlet weak var pantalonMujer: UIImageView!
+    @IBOutlet weak var zapatosMujer: UIImageView!
+    @IBOutlet weak var returnBtn: UIButton!
+
+    // Firestore references
+    let db = Firestore.firestore()
+    let uid = Auth.auth().currentUser?.uid
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        fetchUserData()
     }
-    
-override func viewDidLayoutSubviews() {
+
+    // Function to fetch user data from Firestore
+    func fetchUserData() {
+        if let uid = uid {
+            // Fetch data from rqUsers collection
+            db.collection("rqUsers").document(uid).getDocument { (document, error) in
+                if let document = document, document.exists {
+                    let data = document.data()
+                    if let genero = data?["genero"] as? String,
+                       let puntaje = data?["puntaje"] as? Int,
+                       let prendaI = data?["prendaI"] as? Int,
+                       let prendaS = data?["prendaS"] as? Int {
+                        
+                        // Update UI based on gender
+                        self.updateUIForGender(genero)
+                        
+                        // Assign clothing based on prendaS and prendaI values
+                        self.assignClothing(genero: genero, prendaS: prendaS, prendaI: prendaI)
+
+                        // Fetch data from rqRecompensas collection
+                        self.fetchRecompensasDataAndUpdateFirestore(uid,puntaje)
+                    }
+                } else {
+                    print("Document does not exist")
+                }
+            }
+        }
+    }
+    // Function to assign clothing based on prendaS and prendaI values
+        func assignClothing(genero: String, prendaS: Int, prendaI: Int) {
+            if genero == "Masculino" {
+                // Assign clothing for male avatar
+                switch prendaS {
+                case 1:
+                    self.playeraHombre.image = UIImage(named: "prenda1Shombre")
+                case 2:
+                    self.playeraHombre.image = UIImage(named: "prenda2Shombre")
+                case 3:
+                    self.playeraHombre.image = UIImage(named: "prenda3Shombre")
+                case 4:
+                    self.playeraHombre.image = UIImage(named: "prenda4Shombre")
+                default:
+                    break
+                }
+
+                switch prendaI {
+                case 1:
+                    self.pantalonHombre.image = UIImage(named: "prenda1Ihombre")
+                case 2:
+                    self.pantalonHombre.image = UIImage(named: "prenda2Ihombre")
+                case 3:
+                    self.pantalonHombre.image = UIImage(named: "prenda3Ihombre")
+                default:
+                    break
+                }
+            } else {
+                // Assign clothing for female avatar
+                switch prendaS {
+                case 1:
+                    self.playeraMujer.image = UIImage(named: "prenda1Smujer")
+                case 2:
+                    self.playeraMujer.image = UIImage(named: "prenda2Smujer")
+                case 3:
+                    self.playeraMujer.image = UIImage(named: "prenda3Smujer")
+                case 4:
+                    self.playeraMujer.image = UIImage(named: "prenda4Smujer")
+                default:
+                    break
+                }
+
+                switch prendaI {
+                case 1:
+                    self.pantalonMujer.image = UIImage(named: "prenda1Imujer")
+                case 2:
+                    self.pantalonMujer.image = UIImage(named: "prenda2Imujer")
+                case 3:
+                    self.pantalonMujer.image = UIImage(named: "prenda3Imujer")
+                default:
+                    break
+                }
+            }
+        }
+
+
+    // Function to update UI based on gender
+    func updateUIForGender(_ genero: String) {
+        if genero == "Masculino" {
+            maleAvatar.isHidden = false
+            femaleAvatar.isHidden = true
+            playeraHombre.isHidden = false
+            pantalonHombre.isHidden = false
+            zapatosHombre.isHidden = false
+            playeraMujer.isHidden = true
+            pantalonMujer.isHidden = true
+            zapatosMujer.isHidden = true
+        } else {
+            maleAvatar.isHidden = true
+            femaleAvatar.isHidden = false
+            playeraHombre.isHidden = true
+            pantalonHombre.isHidden = true
+            zapatosHombre.isHidden = true
+            playeraMujer.isHidden = false
+            pantalonMujer.isHidden = false
+            zapatosMujer.isHidden = false
+        }
+    }
+
+    // Function to fetch data from rqRecompensas collection and update Firestore
+    func fetchRecompensasDataAndUpdateFirestore(_ uid: String, _ puntaje: Int) {
+        db.collection("rqRecompensas").document(uid).getDocument { (document, error) in
+            if let document = document, document.exists {
+                var recompensa1 = false
+                var recompensa2 = false
+                var recompensa3 = false
+                var recompensa4 = false
+                var recompensa5 = false
+                
+                if puntaje >= 50 {
+                    recompensa1 = true
+                    }
+
+                if puntaje >= 100 {
+                        recompensa2 = true
+                    }
+                if puntaje >= 200 {
+                        recompensa3 = true
+                    }
+                if puntaje >= 400 {
+                        recompensa4 = true
+                    }
+                if puntaje >= 800 {
+                        recompensa5 = true
+                    }
+                    self.updateRecompensasInFirestore(recompensa1, recompensa2, recompensa3, recompensa4, recompensa5)
+                } else {
+                    print("rqRecompensas document does not exist")
+                }
+            }
+        }
+    // Function to update recompensa states in Firestore
+    func updateRecompensasInFirestore(_ recompensa1: Bool, _ recompensa2: Bool, _ recompensa3: Bool, _ recompensa4: Bool, _ recompensa5: Bool) {
+        if let uid = uid {
+            db.collection("rqUsers").document(uid).updateData([
+                "recompensa1": recompensa1,
+                "recompensa2": recompensa2,
+                "recompensa3": recompensa3,
+                "recompensa4": recompensa4,
+                "recompensa5": recompensa5]) { error in
+                    if let error = error {
+                        print("Error updating recompensa states: \(error.localizedDescription)")
+                    } else {
+                        print("Recompensa states updated successfully")
+                    }
+                }
+            }
+        }
+
+    override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-   //buttons
     }
-//actions
 
+    @IBAction func editAvatarBtnTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "editAvatarSegue", sender: self)
+    }
+
+    @IBAction func returnBtn(_ sender: UIButton) {
+        performSegue(withIdentifier: "homeSegue", sender: self)
+    }
 }
+
--- redquizzios/editAvatarController.swift
+++ redquizzios/editAvatarController.swift
@@ -1,18 +1,214 @@
 
 
 import UIKit
+import Firebase
 
 class editAvatarController: UIViewController {
-//outlets
+    
+    // Outlets
+    @IBOutlet weak var maleAvatar: UIImageView!
+    @IBOutlet weak var femaleAvatar: UIImageView!
+    @IBOutlet weak var playeraHombre: UIImageView!
+    @IBOutlet weak var pantalonHombre: UIImageView!
+    @IBOutlet weak var zapatosHombre: UIImageView!
+    @IBOutlet weak var playeraMujer: UIImageView!
+    @IBOutlet weak var pantalonMujer: UIImageView!
+    @IBOutlet weak var zapatosMujer: UIImageView!
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var superiorBtn: UIButton!
+    @IBOutlet weak var inferiorBtn: UIButton!
+    @IBOutlet weak var saveBtn: UIButton!
+    @IBOutlet weak var returnBtn: UIButton!
+    
+    var userId: String?
+    var genero: String?
+    var puntaje: Int?
+    var prendaS: String?
+    var prendaI: String?
+    var recompensa1: Bool = false
+    var recompensa2: Bool = false
+    var recompensa3: Bool = false
+    var recompensa4: Bool = false
+    var recompensa5: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getUserData()
+        // Configurar visibilidad de UIImageView según el género del usuario
+        configureGenderUI()
+        // Configurar visibilidad de botones según la lógica descrita
+        configureButtonsVisibility()
     }
     
-override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-   //buttons
+    func getUserData() {
+        // Obtener el ID del usuario logeado (puedes implementar esta lógica según tu aplicación)
+        // ...
+        
+        // Consultar Firestore para obtener los datos del usuario
+        let db = Firestore.firestore()
+        let userRef = db.collection("rqUsers").document(userId ?? "")
+        userRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                // Obtener datos del documento
+                self.genero = document["genero"] as? String
+                self.puntaje = document["puntaje"] as? Int
+                self.prendaS = document["prendaS"] as? String
+                self.prendaI = document["prendaI"] as? String
+                
+                // Actualizar visibilidad de UIImageView según el género
+                self.configureGeneroUI()
+                
+                // Actualizar recompensas según el puntaje del usuario
+                self.updateRewards()
+            } else {
+                print("Document does not exist")
+            }
+        }
     }
-//actions
+    func updateRewards() {
+            // Actualizar recompensas según el puntaje del usuario
+            if let puntaje = puntaje {
+                recompensa1 = puntaje >= 50
+                recompensa2 = puntaje >= 100
+                recompensa3 = puntaje >= 200
+                recompensa4 = puntaje >= 400
+                recompensa5 = puntaje >= 800
+            }
+            
+            // Actualizar visibilidad de botones según las recompensas
+            configureButtonsVisibility()
+        }
+        
+        // Consultar Firestore para obtener datos de recompensas
+    let rewardsRef = db.collection("rqRecompensas").document(userId ?? ""); func rewardsRef;.getDocument { (document, error) in
+            if let document = document, document.exists {
+                // Obtener datos de recompensas
+                self.recompensa1 = document["recompensa1"] as? Bool ?? false
+                self.recompensa2 = document["recompensa2"] as? Bool ?? false
+                self.recompensa3 = document["recompensa3"] as? Bool ?? false
+                self.recompensa4 = document["recompensa4"] as? Bool ?? false
+                self.recompensa5 = document["recompensa5"] as? Bool ?? false
+                
+                // Actualizar visibilidad de botones según las recompensas
+                self.configureButtonsVisibility()
+            } else {
+                print("Document does not exist")
+            }
+        }
+    }
 
+func configureGeneroUI() {
+        // Configurar visibilidad de UIImageView según el género del usuario
+        if genero == "Masculino" {
+            maleAvatar.isHidden = false
+            femaleAvatar.isHidden = true
+            playeraHombre.isHidden = false
+            pantalonHombre.isHidden = false
+            zapatosHombre.isHidden = false
+            playeraMujer.isHidden = true
+            pantalonMujer.isHidden = true
+            zapatosMujer.isHidden = true
+        } else if genero == "Femenino" {
+            maleAvatar.isHidden = true
+            femaleAvatar.isHidden = false
+            playeraHombre.isHidden = true
+            pantalonHombre.isHidden = true
+            zapatosHombre.isHidden = true
+            playeraMujer.isHidden = false
+            pantalonMujer.isHidden = false
+            zapatosMujer.isHidden = false
+        }
+    }
+    
+    func configureButtonsVisibility() {
+        // Configurar visibilidad de botones según la lógica descrita
+        btn1.isHidden = false
+        btn2.isHidden = false
+        btn3.isHidden = false
+        btn4.isHidden = false
+    }
+
+    @IBAction func superiorBtnTapped(_ sender: UIButton) {
+        // Habilitar 4 botones
+        btn1.isEnabled = true
+        btn2.isEnabled = reward1
+        btn3.isEnabled = reward3
+        btn4.isEnabled = reward5
+    }
+
+    @IBAction func inferiorBtnTapped(_ sender: UIButton) {
+        // Habilitar 3 botones
+        btn1.isEnabled = true
+        btn2.isEnabled = reward2
+        btn3.isEnabled = reward4
+        btn4.isHidden = true
+    }
+
+    @IBAction func btnTapped(_ sender: UIButton) {
+        // Verificar qué botón fue presionado y actualizar prendaS y la imagen correspondiente
+        if sender == btn1 {
+            prendaS = "1"
+            updateAvatarImage()
+            prendaI = "1"
+            updatePantalonImage()
+        } else if sender == btn2 {
+            prendaS = "2"
+            updateAvatarImage()
+            prendaI = "2"
+            updatePantalonImage()
+        } else if sender == btn3 {
+            prendaS = "3"
+            updateAvatarImage()
+            prendaI = "3"
+            updatePantalonImage()
+        } else if sender == btn4 {
+            prendaS = "4"
+            updateAvatarImage()
+            prendaI = "4"
+            pdatePantalonImage()
+        }
+    }
+    func updateAvatarImage() {
+        // Actualizar la imagen en playeraHombre o playeraMujer según el género y prendaS
+        if userGender == "male" {
+            playeraHombre.image = UIImage(named: "playeraHombre_\(prendaS ?? "1")")
+        } else if userGender == "female" {
+            playeraMujer.image = UIImage(named: "playeraMujer_\(prendaS ?? "1")")
+        }
+    }
+    func updatePantalonImage() {
+        // Actualizar la imagen en pantalonHombre o pantalonMujer según el género y prendaI
+        if userGender == "male" {
+            pantalonHombre.image = UIImage(named: "pantalonHombre_\(prendaI ?? "1")")
+        } else if userGender == "female" {
+            pantalonMujer.image = UIImage(named: "pantalonMujer_\(prendaI ?? "1")")
+        }
+    }
+    @IBAction func saveBtnTapped(_ sender: UIButton) {
+        // Guardar y actualizar en la base de datos el valor de prendaS y prendaI
+            let db = Firestore.firestore()
+            let userRef = db.collection("rqUsers").document(userId ?? "")
+            
+            userRef.updateData([
+                "prendaS": prendaS ?? "",
+                "prendaI": prendaI ?? ""
+            ]) { error in
+                if let error = error {
+                    print("Error updating document: \(error)")
+                } else {
+                    print("Document successfully updated")
+                }
+            }
+
+            // Establecer la selección de imágenes de playera y pantalón según el género
+            if userGender == "male" {
+                playeraHombre.image = UIImage(named: "playeraHombre_\(prendaS ?? "1")")
+                pantalonHombre.image = UIImage(named: "pantalonHombre_\(prendaI ?? "1")")
+            } else if userGender == "female" {
+                playeraMujer.image = UIImage(named: "playeraMujer_\(prendaS ?? "1")")
+                pantalonMujer.image = UIImage(named: "pantalonMujer_\(prendaI ?? "1")")
+    }
 }
